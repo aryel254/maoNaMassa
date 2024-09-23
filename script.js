a92d6c366a395bf83a109d603204411156138510
aryel254 <148634087+aryel254@users.noreply.github.com>
2024-09-23 11:53:01 -0300

    pepa

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var titulo = document.querySelector(".titulo")
 titulo.textContent = ('Aparecida Nutricionista')
 
 var paulo = document.querySelector('#linhaDoPaulo')
-var peso = paulo.querySelector(".info-peso").textContest
-var altura = paulo.querySelector(".info-altura").textContest
+var peso = paulo.querySelector(".info-peso").textContent
+var altura = paulo.querySelector(".info-altura").textContent
 var imc = peso / (altura * altura)
-paulo.querySelector("info-imc").textContest = imc
+paulo.querySelector("info-imc").textContent = imc
